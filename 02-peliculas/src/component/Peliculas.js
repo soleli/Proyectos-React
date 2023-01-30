c78f82b635102957b290828acf658c07b6d65af2
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-30 18:03:36 -0300

    add component Peliculas

---
+++ 02-peliculas/src/component/Peliculas.js
@@ -0,0 +1,14 @@
+function Peliculas() {
+    return (
+        <div className="Pelicula">
+            <h2>Titulo</h2>
+            <p>puntuación</p>
+            <ul>
+                <li>genero1</li>
+                <li>genero2</li>
+            </ul>
+        </div>
+    );
+
+}
+export default Peliculas;
