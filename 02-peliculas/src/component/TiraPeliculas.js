d6f031fc1f860a7e2eb0678438050cb82f3e1d8c
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-30 17:37:50 -0300

    add component TiraPeliculas

---
+++ 02-peliculas/src/component/TiraPeliculas.js
@@ -0,0 +1,40 @@
+function TiraPeliculas(){
+    return(
+        <div>
+            <div className="Pelicula">
+                <h2>Titulo</h2>
+                <p>puntuación</p>
+                <ul>
+                    <li>genero1</li>
+                    <li>genero2</li>
+                </ul>
+            </div>
+            <div className="Pelicula">
+                <h2>Titulo</h2>
+                <p>puntuación</p>
+                <ul>
+                    <li>genero1</li>
+                    <li>genero2</li>
+                </ul>
+            </div>
+            <div className="Pelicula">
+                <h2>Titulo</h2>
+                <p>puntuación</p>
+                <ul>
+                    <li>genero1</li>
+                    <li>genero2</li>
+                </ul>
+            </div>
+            <div className="Pelicula">
+                <h2>Titulo</h2>
+                <p>puntuación</p>
+                <ul>
+                    <li>genero1</li>
+                    <li>genero2</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default TiraPeliculas;
