72be14aa29d485fa8791540f0e2b3b0deb034d17
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-30 18:05:27 -0300

    change TiraPeliculas-  add  component Peliculas

--- 02-peliculas/src/component/TiraPeliculas.js
+++ 02-peliculas/src/component/TiraPeliculas.js
@@ -1,38 +1,12 @@
+import Peliculas from "./Peliculas";
+
 function TiraPeliculas(){
     return(
         <div>
-            <div className="Pelicula">
-                <h2>Titulo</h2>
-                <p>puntuación</p>
-                <ul>
-                    <li>genero1</li>
-                    <li>genero2</li>
-                </ul>
-            </div>
-            <div className="Pelicula">
-                <h2>Titulo</h2>
-                <p>puntuación</p>
-                <ul>
-                    <li>genero1</li>
-                    <li>genero2</li>
-                </ul>
-            </div>
-            <div className="Pelicula">
-                <h2>Titulo</h2>
-                <p>puntuación</p>
-                <ul>
-                    <li>genero1</li>
-                    <li>genero2</li>
-                </ul>
-            </div>
-            <div className="Pelicula">
-                <h2>Titulo</h2>
-                <p>puntuación</p>
-                <ul>
-                    <li>genero1</li>
-                    <li>genero2</li>
-                </ul>
-            </div>
+            <Peliculas/>
+            <Peliculas/>
+            <Peliculas/>
+            <Peliculas/>
         </div>
     )
 }
