29a66bc0b31f07ff5b6d446751c11cebef950802
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-31 10:26:38 -0300

    Add set state in didmount

--- 01-temporizador/src/component/Clock.js
+++ 01-temporizador/src/component/Clock.js
@@ -1,15 +1,27 @@
 import React from "react";
-class Clock extends React.Component{
+class Clock extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state={
-            hora:new Date()
+        this.state = {
+            hora: new Date()
         }
     }
 
-    render(){
-        return(
+    componentDidMount() {
+        setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+    tick() {
+        this.setState({
+            hora: new Date()
+        })
+    }
+
+    render() {
+        return (
             <div>
                 <h1>¡Hola Mundo!</h1>
                 <h2>La hora es: {this.state.hora.toLocaleTimeString()}</h2>
