d9bb8fb8ac36336f859e0293001577ee525a2074
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-31 10:54:35 -0300

    Add content wrapper component

---
+++ 03-dashboard/src/components/ContentWrapper.js
@@ -0,0 +1,13 @@
+
+function ContentWrapper(){
+    return(
+        <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+
+            <TopBar/>
+            <ContentRowTop/>
+            </div>
+        </div>
+    )
+}
+export default ContentWrapper;
