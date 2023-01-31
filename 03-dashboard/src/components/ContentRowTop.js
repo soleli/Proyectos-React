3e44e43f9c4fb799373f6435816513ffd2f46a3e
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-31 19:51:56 -0300

    Add props metrics movies

--- 03-dashboard/src/components/ContentRowTop.js
+++ 03-dashboard/src/components/ContentRowTop.js
@@ -3,12 +3,16 @@ import GenresInDb from "./GenresInDb";
 import LastMoviesInDb from "./LastMovieInDb";
 
 function ContentRowTop() {
+  
     return (
         <div className="container-fluid">
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
             </div>
-            <ContentRowMovies />
+            <div className="row">
+            <ContentRowMovies title="MOVIES IN DATA BASE" quantity="21" icon="fa-film" color="primary" />
+            </div>
+           
             <div className="row">
                 <LastMoviesInDb />
                 <GenresInDb/>
