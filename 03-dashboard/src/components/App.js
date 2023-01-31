5bebed29043dacfc567274b73417b8ba34b5220c
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-31 10:50:48 -0300

    add  sidebar component

---
+++ 03-dashboard/src/components/App.js
@@ -0,0 +1,12 @@
+import '../assets/css/app.css'
+import SideBar from "./SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar/>
+    </div>
+  );
+}
+
+export default App;
