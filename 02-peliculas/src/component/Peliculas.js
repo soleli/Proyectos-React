351907788c79dcb66c721a4d53d5445fcb487fec
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-30 23:36:14 -0300

    change map de props listaDeGeneros

--- 02-peliculas/src/component/Peliculas.js
+++ 02-peliculas/src/component/Peliculas.js
@@ -1,26 +1,34 @@
 import PropTypes from 'prop-types';
 function Peliculas(props) {
+    let listaDeGeneros;
+    if (props.generos != null) {
+        listaDeGeneros = <ul>
+            {props.generos.map((genero, i) => <li key={genero + i}>{genero}</li>)}
+        </ul>
+    }
+    else {
+        listaDeGeneros = "Sin genero"
+    }
     return (
         <div className="Pelicula">
             <h2>{props.titulo}</h2>
             <p>{props.puntuacion}</p>
             <ul>
-                {props.generos.map((genero,i)=><li key={genero+i}>{genero}</li>)}
-                
+                {listaDeGeneros}
             </ul>
         </div>
     );
 
 }
 
-Peliculas.defaultProps={
-    titulo:"Sin titulo",
-    puntuacion:8,
-    generos:["sin genero", "sin genero"]
+Peliculas.defaultProps = {
+    titulo: "Titulo por defecto",
+    puntuacion: 0,
+    generos: ["Género por defecto"]
 }
-Peliculas.propTypes={
-    titulo:PropTypes.string,
-    puntuacion:PropTypes.number,
-    generos:PropTypes.array
+Peliculas.propTypes = {
+    titulo: PropTypes.string,
+    puntuacion: PropTypes.number,
+    generos: PropTypes.array
 }
 export default Peliculas;
