2634be9302e227b5fefe0cb7d6fa3fba4cca7981
Soledad Alaniz <sole_lr@hotmail.com>
2023-01-31 10:19:53 -0300

    change hora props to state

--- 01-temporizador/src/component/Clock.js
+++ 01-temporizador/src/component/Clock.js
@@ -3,12 +3,16 @@ class Clock extends React.Component{
 
     constructor(props) {
         super(props);
+        this.state={
+            hora:new Date()
+        }
     }
+
     render(){
         return(
             <div>
                 <h1>¡Hola Mundo!</h1>
-                <h2>La hora es: {this.props.hora.toLocaleTimeString()}</h2>
+                <h2>La hora es: {this.state.hora.toLocaleTimeString()}</h2>
             </div>
         );
     }
