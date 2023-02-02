94ce3c0b73fdce7006a50c238a00980853f4686e
Soledad Alaniz <sole_lr@hotmail.com>
2023-02-02 08:41:11 -0300

    update props metrics using array and map

--- 03-dashboard/src/components/ContentRowTop.js
+++ 03-dashboard/src/components/ContentRowTop.js
@@ -3,14 +3,34 @@ import GenresInDb from "./GenresInDb";
 import LastMoviesInDb from "./LastMovieInDb";
 
 function ContentRowTop() {
-  
+  let metrics=[
+    {
+        title:"MOVIES IN DATA BASE",
+        quantity:"21",
+        icon:"fa-film",
+        color:"primary" 
+    },
+    {
+        title:"TOTAL AWARDS",
+        quantity:"79",
+        icon:"fa-award",
+        color:"success" 
+    },
+    {
+        title:"ACTORS QUANTITY",
+        quantity:"49",
+        icon:"fa-user",
+        color:"warning" 
+    }
+
+  ]
     return (
         <div className="container-fluid">
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
             </div>
             <div className="row">
-            <ContentRowMovies title="MOVIES IN DATA BASE" quantity="21" icon="fa-film" color="primary" />
+                {metrics.map(metric=><ContentRowMovies title={metric.title} quantity={metric.quantity} icon={metric.icon} color={metric.color} />)}
             </div>
            
             <div className="row">
