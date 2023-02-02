88d3bb3161170473d3894f978086b765e7525dfe
Soledad Alaniz <sole_lr@hotmail.com>
2023-02-02 09:14:52 -0300

    Change tirapeliculas from stateless to stateful

--- 02-peliculas/src/component/TiraPeliculas/TiraPeliculas.js
+++ 02-peliculas/src/component/TiraPeliculas/TiraPeliculas.js
@@ -1,17 +1,19 @@
 import Peliculas from "../Peliculas/Peliculas";
+import { Component } from "react";
+class TiraPeliculas extends Component {
+    render() {
+        return (
+            <div>
+                <Peliculas titulo="Inception" puntuacion="8.0" generos={["Ciencia ficción", "Thriller", "Intriga", "Acción"]} />
+                <Peliculas titulo="Avatar " puntuacion="7.2" generos={["Ciencia ficción", "Aventuras"]} />
+                <Peliculas titulo="Joker" puntuacion="8.0" generos={["Thriller", "Drama"]} />
+                <Peliculas titulo="The Dark Knight" puntuacion="8.1" generos={["Thriller", "Drama", "Acción"]} />
+                <Peliculas />
 
-function TiraPeliculas(){
-    return(
-        <div>
-            <Peliculas titulo="Inception" puntuacion="8.0" generos={["Ciencia ficción","Thriller","Intriga","Acción"]}/>
-            <Peliculas titulo="Avatar " puntuacion="7.2" generos={["Ciencia ficción","Aventuras"]}/>
-            <Peliculas titulo="Joker" puntuacion="8.0" generos={["Thriller","Drama"]}/>
-            <Peliculas titulo="The Dark Knight" puntuacion="8.1" generos={["Thriller","Drama","Acción"]}/>
-            <Peliculas/>
-         
-            
-        </div>
-    )
+
+            </div>
+        )
+    }
 }
 
 export default TiraPeliculas;
