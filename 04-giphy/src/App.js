7ca9c040e1b6d8133ce835a097247c6fb0b6c649
Soledad Alaniz <sole_lr@hotmail.com>
2023-02-07 09:44:39 -0300

    Add api call

--- 04-giphy/src/App.js
+++ 04-giphy/src/App.js
@@ -4,8 +4,20 @@ import Nav from './components/Nav';
 import { Component } from 'react';
 
 class App extends Component {
-  render() {
 
+  apiCall(url, consecuencia) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => consecuencia(data))
+      .catch(err => console.log(err))
+  }
+  mostrarGif = (data) => {
+    console.log(data);
+  }
+  componentDidMount() {
+    this.apiCall("https://api.giphy.com/v1/gifs/random?api_key=nIq9C1NnwmZ3C3rYnVDbCT0M4bZAnyzN&tag=&rating=g", this.mostrarGif)
+  }
+  render() {
     return (
       <div>
         <Nav />
