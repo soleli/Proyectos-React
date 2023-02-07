a0390be9d11d45e95d3055c7d90938a185d00dbd
Soledad Alaniz <sole_lr@hotmail.com>
2023-02-07 09:19:55 -0300

    Add gif component

--- 04-giphy/src/App.js
+++ 04-giphy/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Gif from './components/Gif';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
-       <Nav/>
+      <Nav />
+      <div className="container">
+        <div className="row text-center">
+          <Gif/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ 04-giphy/src/components/Gif.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+
+class Gif extends Component {
+
+    render() {
+        return (
+
+
+            <div className="col-lg-3 col-md-6 mb-4">
+                <div className="card h-100">
+                    <img className="card-img-top" src="" alt="" />
+                    <div className="card-body">
+                        <h4 className="card-title">Titulo</h4>
+                    </div>
+                </div>
+            </div>
+
+
+
+        )
+    }
+}
+
+export default Gif;
