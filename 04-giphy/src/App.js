52a0c033f4648df908e549743ddc59e611a625ce
Soledad Alaniz <sole_lr@hotmail.com>
2023-02-07 09:26:40 -0300

    Change App function to class

--- 04-giphy/src/App.js
+++ 04-giphy/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import Gif from './components/Gif';
 import Nav from './components/Nav';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Nav />
-      <div className="container">
-        <div className="row text-center">
-          <Gif/>
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <Nav />
+        <div className="container">
+          <div className="row text-center">
+            <Gif />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
