3e28bb0fa1b8adbe58dbba348612abd0fc820b1a
Soledad Alaniz <sole_lr@hotmail.com>
2023-03-02 01:27:34 -0300

    Add listadopacientes component

--- admin-pacientes/src/App.jsx
+++ admin-pacientes/src/App.jsx
@@ -1,10 +1,14 @@
+import Formulario from "./components/Formulario"
 import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
  
   return (
     <div >
      <Header/>
+     <Formulario/>
+     <ListadoPacientes/>
        
     </div>
   )
---
+++ admin-pacientes/src/components/ListadoPacientes.jsx
@@ -0,0 +1,8 @@
+
+const ListadoPacientes = () => {
+  return (
+    <h2>Listado de Pacientes</h2>
+  )
+}
+
+export default ListadoPacientes
